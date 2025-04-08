93a22d10a74d7d1db2c5e2bf995b131a3c4f47a5
Damkai31 <dgurka@ucsc.edu>
2025-04-07 18:19:22 -0700

    Add files via upload

---
+++ float.py
@@ -0,0 +1,20 @@
+# Sum of two float numbers
+float1 = 4.6
+float2 = 3.8
+sum_result = float1 + float2
+print("Sum:", sum_result)
+print("Type of sum_reslut:", type(sum_result))
+
+#Diff between two integers
+int1 = 10
+int2 = 4
+diff_result = int1 - int2
+print("Difference:", diff_result)
+print("Type of diff_result:", type(diff_result))
+
+#Product of floating point and int
+float3 = 5.5
+int3 = 2
+product_result = float3 * int3
+print("Product:", product_result)
+print("Type of product_result:", type(product_result))
